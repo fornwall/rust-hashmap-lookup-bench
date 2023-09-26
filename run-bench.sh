c314e4e0f67a9bb3ae6b5fc22a0bf3e798442cf0
Fredrik Fornwall <fredrik@fornwall.net>
2023-09-26 23:41:04 +0200

    Use run-bench.sh in CI

--- run-bench.sh
+++ run-bench.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
 echo "# Native run"
-cargo bench
+cargo bench -q
+
+echo "# Native run (RUSTFLAGS='-C target-cpu=native')"
+RUSTFLAGS='-C target-cpu=native' cargo bench -q
 
 echo "# Wasmtime run"
-CARGO_TARGET_WASM32_WASI_RUNNER="wasmtime --dir=." cargo wasi bench
+CARGO_TARGET_WASM32_WASI_RUNNER="wasmtime --dir=." cargo wasi bench -q
 
 echo "# Wasmer run"
-CARGO_TARGET_WASM32_WASI_RUNNER="wasmer --dir=." cargo wasi bench
+CARGO_TARGET_WASM32_WASI_RUNNER="wasmer --dir=." cargo wasi bench -q
